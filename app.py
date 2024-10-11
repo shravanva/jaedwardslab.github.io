8ff8b9cae1673a857be20eb68eddfe253a9279b2
shravanva <shravan_parunandi@tamu.edu>
2024-10-11 14:03:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, jsonify, url_for
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -6,58 +6,38 @@ import numpy as np
 import os
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pheno_file = request.files['phenotype']
-        allele_file = request.files['allele']
+        phenotype_file = request.files.get('phenotype_file')
+        allele_file = request.files.get('allele_file')
+        
+        if not phenotype_file or not allele_file:
+            return jsonify({'success': False, 'error': 'Please upload both files!'})
 
-        if pheno_file and allele_file:
-            pheno_path = os.path.join(UPLOAD_FOLDER, pheno_file.filename)
-            allele_path = os.path.join(UPLOAD_FOLDER, allele_file.filename)
+        try:
+            # Save files temporarily
+            phenotype_filepath = os.path.join("uploads", phenotype_file.filename)
+            allele_filepath = os.path.join("uploads", allele_file.filename)
+            phenotype_file.save(phenotype_filepath)
+            allele_file.save(allele_filepath)
 
-            pheno_file.save(pheno_path)
-            allele_file.save(allele_path)
+            # Load data using pandas
+            pheno = pd.read_csv(phenotype_filepath, sep="\t")
+            allele_data = pd.read_csv(allele_filepath, sep=",", quotechar='"')
 
-            # Run the analysis and plot generation
-            plot_path1 = generate_plots(pheno_path, allele_path)
+            # Data processing and plotting code...
+            plt.figure(figsize=(10, 6))
+            sns.violinplot(data=pheno, x='some_column', y='another_column')
+            plt.savefig('static/plot1.jpg', format='jpeg')
 
-            return render_template('result.html', plot1=plot_path1)
+            return jsonify({'success': True, 'image_url': url_for('static', filename='plot1.jpg')})
+
+        except Exception as e:
+            return jsonify({'success': False, 'error': str(e)})
 
     return render_template('index.html')
-
-def generate_plots(pheno_file, allele_file):
-    # Load phenotype and allele data
-    pheno = pd.read_csv(pheno_file, sep="\t")
-    allele_data = pd.read_csv(allele_file, sep=",", quotechar='"')
-
-    # Rename columns and merge data
-    allele_data.rename(columns={'strain': 'Accession_ID', 'alt': 'alt'}, inplace=True)
-    snp_pheno = pd.merge(pheno, allele_data[['Accession_ID', 'alt']], on='Accession_ID', how='left')
-    snp_pheno['allele'] = np.where(snp_pheno['alt'] == 'C', 'minor', 'major')
-    snp_pheno['allele'].fillna('major', inplace=True)
-    snp_pheno['treatment'] = pd.Categorical(snp_pheno['treatment'], categories=['Mock', 'ABA'], ordered=True)
-
-    # Plotting Half Violin Plot
-    plt.figure(figsize=(10, 6))
-    sns.violinplot(data=snp_pheno, x='treatment', y='Ave_RLN', hue='treatment', split=True, inner=None)
-    sns.boxplot(data=snp_pheno, x='treatment', y='Ave_RLN', width=0.05, showcaps=False,
-                boxprops={'facecolor': 'white', 'edgecolor': 'black', 'linewidth': 2},
-                meanprops={'color': 'red', 'linewidth': 2},
-                palette={'Mock': 'orange', 'ABA': 'skyblue'})
-
-    for _, group_data in snp_pheno.groupby('Accession_ID'):
-        plt.plot(group_data['treatment'], group_data['Ave_RLN'], color='grey', alpha=0.3)
-
-    plt.title('Half Violin Plot of Ave_RLN by Treatment')
-    violin_plot_path = os.path.join('static', 'violin_plot.jpg')
-    plt.savefig(violin_plot_path, format='jpeg')
-    plt.close()
-
-    return violin_plot_path
 
 if __name__ == '__main__':
     app.run(debug=True)
